9c515784c98aba11043743935ee3156dcfc0c975
Ron-sagiv <pedrozeeg@gmail.com>
2026-02-19 20:52:53 +0100

    Added offline options

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 // import './index.css'
 import * as atatus from 'atatus-spa';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 atatus.config('73f9eb1b2acd4832a4c5482d06693b21').install();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -10,6 +12,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 );
-
+serviceWorkerRegistration.register();
 //=========this is not supposed to be here but I just try
 // atatus.notify(new Error('Test Atatus Setup'));
